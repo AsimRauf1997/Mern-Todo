70eea7712e51fce2d063d4163160669318c40622
asimrauf-sybrid <102941062+asimrauf-sybrid@users.noreply.github.com>
2022-06-18 14:35:33 +0500

    changes done

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,54 +1,70 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-const Tasks = ({deleteTask, updateTask }) => {
-  const [data, setData] = useState([])
+const Tasks = ({ deleteTask, updateTask }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const getTasks = () => {
     try {
-      fetch('http://localhost:8001/api/todo/', {
-        method: 'GET',
-      }).then(function(response) {
-        return response.json();
-      }).then(function(data){
-        setData(data)
-
+      fetch("http://localhost:8001/api/todo/", {
+        method: "GET",
       })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          setData(data);
+          setLoading(false);
+        });
     } catch (error) {
-      alert(error)
-      
+      alert(error);
     }
-    
-}
+  };
 
-useEffect(()=>{
-  getTasks()
-},[data])
+  useEffect(() => {
+    getTasks();
+  }, [data]);
 
   return (
     <div className='task-continer'>
       <h2> My Tasks</h2>
-      {data.map((task) => (
-        <div  key={task._id}>
-        <label>Title</label>
-        <p className="task">{task.title}</p>
-        <label>Description</label>
-        <p className="task" >{task.description}</p>
-        {task.createdAt && (
-          <>
-          <label>CreatedAt</label>
-          <p className="task" >{task.createdAt.substr(0, 10)}</p>
-          </>
-        )}
-        
-        <div className='btn-container'>
-        <button className='btn' onClick={()=> updateTask(task._id,task.title, task.description)}>Edit</button>
-        <button className='btn' onClick={()=>deleteTask(task._id)}>Delete</button>
+      {loading ? (
+        <div className='loader'>
+          <h3>Loading....</h3>
         </div>
+      ) : (
+        <>
+          {data.map((task) => (
+            <div key={task._id}>
+              <label>Title</label>
+              <p className='task'>{task.title}</p>
+              <label>Description</label>
+              <p className='task'>{task.description}</p>
+              {task.createdAt && (
+                <>
+                  <label>CreatedAt</label>
+                  <p className='task'>{task.createdAt.substr(0, 10)}</p>
+                </>
+              )}
 
-        </div>
-        
-      ))}
+              <div className='btn-container'>
+                <button
+                  className='btn'
+                  onClick={() =>
+                    updateTask(task._id, task.title, task.description)
+                  }
+                >
+                  Edit
+                </button>
+                <button className='btn' onClick={() => deleteTask(task._id)}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
