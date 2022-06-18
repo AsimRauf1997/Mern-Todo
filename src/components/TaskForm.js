df6c8dd74461683e9d674246c0adc0a46f1f35af
asimrauf-sybrid <102941062+asimrauf-sybrid@users.noreply.github.com>
2022-06-18 13:31:15 +0500

    changes done

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,84 +1,105 @@
-import React, { useState } from 'react';
-import '../App.css';
-import Tasks from './Tasks';
+import React from "react";
+import "../App.css";
+import Tasks from "./Tasks";
 
-const TaskForm = ({title, setTitle, description, setDescription, isUpdating, setIsUpdating, id, setId}) => {
-   const addUpdateTask = () => {
-        if(isUpdating===false){
-          fetch('http://localhost:8001/api/todo/', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-type': 'application/json'
-              },
-            body: JSON.stringify({
-                title,
-                description
-            })
-          }).then(function(response) {
-            console.log('Response',response)
-            setTitle('')
-            setDescription('')
-            return response.json();
-          }).catch(function(err){
-              alert(err)
-          })
-        } else{
-          fetch(`http://localhost:8001/api/todo/${id}`, {
-          method: 'PUT',
-          mode: 'cors',
-          headers: {
-              'Content-type': 'application/json'
-            },
-            body:JSON.stringify({
-              title,
-              description
-            })
-        }).then(function(response) {
-          setTitle('')
-          setDescription('')
-          setIsUpdating('')
+const TaskForm = ({
+  title,
+  setTitle,
+  description,
+  setDescription,
+  isUpdating,
+  setIsUpdating,
+  id,
+  setId,
+}) => {
+  const addUpdateTask = () => {
+    if (isUpdating === false) {
+      fetch("http://localhost:8001/api/todo/", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+        }),
+      })
+        .then(function (response) {
+          console.log("Response", response);
+          setTitle("");
+          setDescription("");
           return response.json();
+        })
+        .catch(function (err) {
+          alert(err);
         });
-        }
-    }
-    const deleteTask = (id) => {
+    } else {
       fetch(`http://localhost:8001/api/todo/${id}`, {
-          method: 'DELETE',
-        }).then(function(response) {
-          console.log('Response',response)
-          return response.json();
-        });
+        method: "PUT",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+        }),
+      }).then(function (response) {
+        setTitle("");
+        setDescription("");
+        setIsUpdating("");
+        return response.json();
+      });
     }
-    const updateTask = (id,title, description) => {
-      setIsUpdating(true)
-      setId(id)
-      setTitle(title)
-      setDescription(description)
-    }
+  };
+  const deleteTask = (id) => {
+    fetch(`http://localhost:8001/api/todo/${id}`, {
+      method: "DELETE",
+    }).then(function (response) {
+      console.log("Response", response);
+      return response.json();
+    });
+  };
+  const updateTask = (id, title, description) => {
+    setIsUpdating(true);
+    setId(id);
+    setTitle(title);
+    setDescription(description);
+  };
   return (
     <>
-    <form className='form-container' action='/' onSubmit={addUpdateTask} >
+      <form className='form-container' action='/' onSubmit={addUpdateTask}>
         <h2> Add New Task</h2>
-      <div className="form-control">
-              <label htmlFor='title'>Title</label>
-              <input type="text" name='title' id="title" placeholder='Enter New Todo' value={title} onChange={e => setTitle(e.target.value)}></input>
-              <label htmlFor='description'>Description</label>
-              <input type="text" name='description' id="description" placeholder='Enter New Todo' value={description} onChange={e => setDescription(e.target.value)}></input>   
-         </div>
-         <div className='btn-container'>
-         <button className='btn' type='submit'>{isUpdating? 'Update': 'Add Task'}</button>
-
-         </div>
+        <div className='form-control'>
+          <label htmlFor='title'>Title</label>
+          <input
+            type='text'
+            name='title'
+            id='title'
+            placeholder='Enter New Todo'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+          <label htmlFor='description'>Description</label>
+          <input
+            type='text'
+            name='description'
+            id='description'
+            placeholder='Enter New Todo'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></input>
+        </div>
+        <div className='btn-container'>
+          <button className='btn' type='submit'>
+            {isUpdating ? "Update" : "Add Task"}
+          </button>
+        </div>
       </form>
-      <Tasks deleteTask={deleteTask} updateTask={updateTask}/>
+      <Tasks deleteTask={deleteTask} updateTask={updateTask} />
     </>
-    
-  )
-}
+  );
+};
 
-export default TaskForm
-
-
-
-
+export default TaskForm;
